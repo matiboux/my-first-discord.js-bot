a37cace903664710361fcb970abd3a373dbc5c4f
Matiboux <matiboux@gmail.com>
2020-01-30 20:06:02 +0000

    Updated main.js to use the prefix

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+// Bot Discord client
 const Discord = require('discord.js');
-var Config = require('./config.json');
 const client = new Discord.Client();
 
+// Bot Configuration
+const config = require('./config.json');
+const token = config.token, prefix = config.prefix;
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	if (msg.content === `${prefix}ping`) {
+		msg.channel.send('Pong.');
+	} else if (msg.content === `${prefix}beep`) {
+		msg.channel.send('Boop.');
+	} else if (msg.content === `${prefix}server`) {
+		msg.channel.send(`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
+	} else if (msg.content === `${prefix}user-info`) {
+		msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
+	}
 });
 
-client.login(Config.token);
+// Run the discord bot
+client.login(token);
