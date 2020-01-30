7462c0a7ae836fb0b7df272490af344364741710
Matiboux <matiboux@gmail.com>
2020-01-30 20:38:51 +0000

    Added more example commands!

--- main.js
+++ main.js
@@ -35,6 +35,39 @@ client.on('message', msg => {
 
 		msg.channel.send(`Arguments (${args.length}): ${args}`);
 	}
+	else if (command === 'kick') {
+		if (!msg.mentions.users.size)
+			return msg.reply(`${msg.author}, you need to tag a user in order to kick them!`);
+		
+		const taggedUser = msg.mentions.users.first();
+		msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	}
+	else if (command === 'avatar') {
+		if (!msg.mentions.users.size)
+			return msg.channel.send(`Your avatar: <${msg.author.displayAvatarURL}>`);
+
+		const avatarList = msg.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+		});
+
+		// Send the array of strings as a message (joined by \n)
+		msg.channel.send(avatarList);
+	}
+	else if (command === 'prune') {
+		const amount = parseInt(args[0]) + 1;
+
+		if (isNaN(amount)) {
+			return msg.reply('that doesn\'t seem to be a valid number.');
+		}
+		else if (amount < 1 || amount > 100) {
+			return msg.reply('you need to input a number between 2 and 100.');
+		}
+
+		msg.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			msg.channel.send('there was an error trying to prune messages in this channel!');
+		});
+	}
 });
 
 // Run the discord bot
