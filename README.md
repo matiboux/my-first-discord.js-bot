ac80a93da84a7198d9da4bcf4fe58a51c2d14a61
Matiboux <matiboux@gmail.com>
2020-01-30 19:44:37 +0000

    Added files for a basic connected bot

---
+++ README.md
@@ -0,0 +1,16 @@
+# My first Discord.js bot
+
+<img src="https://discordapp.com/assets/e4923594e694a21542a489471ecffa50.svg" width="200" />
+
+My first working Discord bot using the discord.js library.
+
+## Dependencies
+
+- Discord.js
+
+## Run
+
+```
+npm install
+npm start
+```
---
+++ main.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+var Config = require('./config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(Config.token);
