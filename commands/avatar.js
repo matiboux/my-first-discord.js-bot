e06ee5e04280b0275cbbc75145a3d4bd80cef773
Matiboux <matiboux@gmail.com>
2020-01-30 22:53:34 +0000

    Added aliases, cooldowns, and checks (guildOnly, args)

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'avatar',
 	description: 'Get avatar URLs of requested users',
+	aliases: ['icon', 'pfp'],
 	execute(message, args) {
 		if (!message.mentions.users.size)
 			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
