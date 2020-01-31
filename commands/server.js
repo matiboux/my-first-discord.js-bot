9c6450c0a7481b65e91208d5103d3ac732ce6e13
Matiboux <matiboux@gmail.com>
2020-01-31 22:20:46 +0000

    Added { guildOnly: true } to server command

--- commands/server.js
+++ commands/server.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'server',
 	description: 'General information about the server',
+	guildOnly: true,
 	execute(message, args) {
 		message.channel.send(`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
 	},
