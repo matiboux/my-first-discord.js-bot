3f657ee04170d20313005aded63e013406f4913a
Matiboux <matiboux@gmail.com>
2020-02-01 19:25:19 +0000

    Added isadmin command

---
+++ commands/isadmin.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'isadmin',
+	description: 'Check if a user is admin',
+	guildOnly: true,
+	usage: '[user]',
+	execute(message, args) {
+		if (!message.mentions.members.size)
+		{
+			if (message.member.hasPermission('ADMINISTRATOR'))
+				return message.reply('You are an Administrator!');
+			
+			return message.reply('You are not an Administrator.');
+		}
+
+		const member = message.mentions.members[0];
+
+		if (message.mentions.members.every(member => member.hasPermission('ADMINISTRATOR')))
+			return message.reply('They are an Administrator!');
+		
+		return message.reply('They are not an Administrator.' + message.mentions.members.size);
+	},
+};
